bfe3e333ba291f52682553f792778a253f769886
Namra00 <namra.d.29@gmail.com>
2019-12-20 02:21:30 -0500

    Setting up Django and initiating paths and views

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project Title
+
+This project includes use of very popular MVC framework Django which uses purely python to develop full stack website.
+
+## Getting Started
+
+work in progress
+
+### Prerequisites
+
+What things you need to install the software and how to install them
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import httpResponse
+from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return httpResponse("<h1>Welcome to Django</h1>")
+    return HttpResponse("<h1>Welcome to Django</h1>")
 
 def v1(response):
-    return httpResponse("<h2>View 1<h2>")
+    return HttpResponse("<h2>View 1<h2>")
