77bbd287acb62c7eeb9614d8656a7ada18519ab4
Namra Desai <namra.d.29@gmail.com>
2019-12-21 03:36:54 -0500

    Added templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
-
-
+posts = [
+    {
+        'author' : 'Namra Desai',
+        'title' : 'Blog Post 1',
+        'content' : 'first post content',
+        'date_posted' : 'December 20,2019'
+    },
+    {
+        'author' : 'Poojan Patel',
+        'title' : 'Blog Post 2',
+        'content' : 'second post content',
+        'date_posted' : 'December 21,2019'
+    },
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>About Blog</h1>')
+    return render(request,'blog/about.html',{'title':'About'})
