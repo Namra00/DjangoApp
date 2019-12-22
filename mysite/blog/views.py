9e7c53b4c2f4474abf15028c1f2d7e025fd8363f
Namra Desai <namra.d.29@gmail.com>
2019-12-21 22:26:22 -0500

    added admin and featured database

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
-posts = [
-    {
-        'author' : 'Namra Desai',
-        'title' : 'Blog Post 1',
-        'content' : 'first post content',
-        'date_posted' : 'December 20,2019'
-    },
-    {
-        'author' : 'Poojan Patel',
-        'title' : 'Blog Post 2',
-        'content' : 'second post content',
-        'date_posted' : 'December 21,2019'
-    },
-]
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
