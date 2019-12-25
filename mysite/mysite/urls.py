06f205d6beb37911d059b94843a5b8751f10c0b0
Namra00 <namra.d.29@gmail.com>
2019-12-25 17:41:36 -0500

    featuring login and logout

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from users import views as user_views
 
@@ -22,4 +23,7 @@ urlpatterns = [
     path('', include("main.urls")),
     path('blog/',include('blog.urls')),
     path('blog/register/',user_views.register,name='register'),
+    path('blog/login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('blog/logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('blog/profile/',user_views.profile,name='profile'),
 ]
