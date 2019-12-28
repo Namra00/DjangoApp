a34b1ac7de66fbc3a08f1d993f297e49e8d50f9c
Namra00 <namra.d.29@gmail.com>
2019-12-27 22:43:36 -0500

    Minor correction on queryset

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -23,7 +23,7 @@ class UserPostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
 
-    def get_query_set(self):
+    def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
 
