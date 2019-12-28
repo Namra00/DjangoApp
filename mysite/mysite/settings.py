d7896de1b87f4f4bf4f88104e421e35ea5e7227e
Namra00 <namra.d.29@gmail.com>
2019-12-28 16:41:27 -0500

    Adding resetting the password feature

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,3 +129,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     path('blog/register/',user_views.register,name='register'),
     path('blog/login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('blog/logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('blog/password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset_done'),
+    path('blog/password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset'),
+    path('blog/password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('blog/password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
     path('blog/profile/',user_views.profile,name='profile'),
 ]
 
